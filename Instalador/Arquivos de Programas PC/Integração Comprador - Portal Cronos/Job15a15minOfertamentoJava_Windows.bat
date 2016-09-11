d9d5e44b66498c251c3eeedccc298089283532f7
Eric <Eric@172.31.12.155>
2016-09-11 17:41:30 -0300

    Caminho redundante rt.jar removido do manifest classpath entry

--- Instalador/Arquivos de Programas PC/Integração Comprador - Portal Cronos/Job15a15minOfertamentoJava_Windows.bat
+++ Instalador/Arquivos de Programas PC/Integração Comprador - Portal Cronos/Job15a15minOfertamentoJava_Windows.bat
@@ -4,7 +4,9 @@ set CLASSPATH=
 
 set path=%path%;C:\Program Files\Java\jre1.8.0_92\bin
 
-set CLASSPATH=%CLASSPATH%;C:\Program Files\Java\jre1.8.0_92\lib\rt.jar
+REM http://stackoverflow.com/questions/23730887/why-is-rt-jar-not-part-of-the-class-path-system-property : 
+REM 	"rt.jar doesn't need to be in the classpath, since it is already in the bootclasspath. It is safe to remove it from your classpath."
+REM set CLASSPATH=%CLASSPATH%;C:\Program Files\Java\jre1.8.0_92\lib\rt.jar
 
 set CLASSPATH=%CLASSPATH%;jersey-bundle-1.12.jar
 set CLASSPATH=%CLASSPATH%;jersey-core-1.12.jar
