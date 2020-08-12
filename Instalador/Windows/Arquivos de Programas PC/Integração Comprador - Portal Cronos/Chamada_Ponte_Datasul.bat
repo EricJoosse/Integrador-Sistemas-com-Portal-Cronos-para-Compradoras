5c4504ec049f0ab8d37c341b735f41e4adbdde6e
eric <eric@Desenv01.v5e55e1oujdu5csnym2xtqjvra.bx.internal.cloudapp.net>
2020-08-12 15:13:08 -0300

    Testador Unitário da ponte Java

---
+++ Instalador/Windows/Arquivos de Programas PC/Integração Comprador - Portal Cronos/Chamada_Ponte_Datasul.bat
@@ -0,0 +1,12 @@
+D:
+cd\
+cd totvs
+cd projetos-datasul11
+cd ccp0001
+cd Includes
+
+REM Antigo - com diretórios flexíveis:
+REM "%JAVA_HOME%\bin\java -cp ponte-integracao-pcronos-2.1.0.jar pcronos.integracao.comprador.PonteWebServicesPortalCronos 'www.portalcronos.com.br:81\\v2\\api\\Requisicao\\PostFileRequisicao' ws-sjcc 123456 'c:\\temp\\req_10798130000175_243680_20200724_145856.xml' 'c:\\temp\\Ret_10798130000175_243680_20200724_145856.xml' 'c:\\temp\\Status_10798130000175_243680_20200724_145856.txt'
+ 
+REM Versão atual - com único diretório fixo:
+"%JAVA_HOME%\bin\java -cp ponte-integracao-pcronos-2.1.0.jar pcronos.integracao.comprador.PonteWebServicesPortalCronos 'www.portalcronos.com.br:81\\v2\\api\\Requisicao\\PostFileRequisicao' ws-sjcc 123456 'req_10798130000175_243680_20200724_145856.xml' 'Ret_10798130000175_243680_20200724_145856.xml' 'Status_10798130000175_243680_20200724_145856.txt'
